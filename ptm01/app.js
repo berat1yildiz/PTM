c83a9bfb1f50793f3a3e262ef78acf445b5876da
berat1yildiz <berat.yildiz@useinsider.com>
2022-12-09 15:40:08 +0300

    fix

--- ptm01/app.js
+++ ptm01/app.js
@@ -1,50 +1,45 @@
-
-var recorderData = [];
-var recorderData2 = [];
+var audioData = [];
+var recordedData = [];
 var isRecordOn = false;
 
 function playSound(button_id){
-    
     var audio = new Audio(button_id + ".mp3");
 
-    recorderData.push(button_id);
+    audioData.push(button_id);
+
     audio.play();
 }
 
 function recorder(){    
-    
     if(!isRecordOn){
         $('#record').css('color', 'red');
-        isRecordOn = true;
-        recorderData = [];
 
+        isRecordOn = true;
+        audioData = [];
     }else{
         $('#record').css('color', 'black');
-        isRecordOn = false;
-        recorderData2 = recorderData;
-    }
-    
 
+        isRecordOn = false;
+        recordedData = audioData;
+    }
 }
 
 function play(index) {
-    var audio = new Audio(recorderData2[index] + ".mp3");
+    var audio = new Audio(recordedData[index] + ".mp3");
 
-    console.log(recorderData2[index] + " playing.."); 
+    console.log(recordedData[index] + " playing.."); 
 
     audio.play();
 
     setTimeout(function(){
-        recorderData2[index+1] && play(index+1);
+        recordedData[index+1] && play(index+1);
     }, 6000)
 }
 
 function checkSound() {
-
-    recorderData2.length>0 && play(0);
+    recordedData.length>0 && play(0);
 }
 
 function deleteRecording(){
-
-    recorderData2 = [];
+    recordedData = [];
 }
